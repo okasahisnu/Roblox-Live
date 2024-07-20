a832a13b139dbc10b4e5b51ac57141aa589b5088
Okasahisnu <okasahisnu@gmail.com>
2024-07-20 10:46:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000; // Use the PORT environment variable or f
 
 app.use(cors());
 
-const tiktokUsername = process.env.TIKTOK_USERNAME || 'fahrezaos';
+const tiktokUsername = process.env.TIKTOK_USERNAME || 'dnhal';
 let tiktokLiveConnection = new WebcastPushConnection(tiktokUsername);
 
 // Cache for profile pictures to avoid frequent requests
